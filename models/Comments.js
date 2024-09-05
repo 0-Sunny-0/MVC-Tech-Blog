7263713b66a7adfc7d21f37f79050b1f68e7830a
Sunny Le Bunz <msafinch@gmail.com>
2024-09-05 14:16:16 -0400

    models continued

--- models/Comments.js
+++ models/Comments.js
@@ -4,4 +4,37 @@ const Blog = require('./Blog');
 
 class Comments extends Model {}
 
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        commenter: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        comment: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Blog,
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        timestamps: false,
+        modelName: 'comments',
+    }
+);
+
 module.exports = Comments;
--- models/User.js
+++ models/User.js
@@ -3,4 +3,33 @@ const sequelize = require('../config/connection');
 
 class User extends Model {}
 
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        pass: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        timestamps: false,
+        modelName: 'user',
+    }
+);
+
 module.exports = User;
